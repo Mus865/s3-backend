df56e4fe26705ff10bca50d81306f84fea57677f
mustapha garba <musgab3@gmail.com>
2023-10-09 13:39:22 -0400

    m2

--- release.config.js
+++ release.config.js
@@ -2,9 +2,8 @@ module.exports = {
     branches: "main",
     repositoryUrl: "https://github.com/Mus865/s3-backend.git",
     plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/npm",
-      "@semantic-release/github",
-    ] 
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
+      '@semantic-release/github'] 
   };
